b942d4f39ba80128da29850dead60bb61ecf2b31
Evgen-skyline <evgen-skyline@ukr.net>
2016-03-30 22:09:26 +0300

    редчайший говнокод

--- app/src/main/java/evgenskyline/exchangerates/MainActivity.java
+++ app/src/main/java/evgenskyline/exchangerates/MainActivity.java
@@ -1,5 +1,6 @@
 package evgenskyline.exchangerates;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -13,6 +14,8 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -25,35 +28,115 @@ import java.nio.channels.ReadableByteChannel;
 import java.net.HttpURLConnection;
 import java.io.*;
 import java.util.HashMap;
+//AppCompatActivity
+public class MainActivity extends Activity {
+    public class MoneyBox{
+        public String someThing;
+        public String ukrName;
+        public String rate;
+        public String name;
+        public String exchangedate;
+    }
 
-public class MainActivity extends AppCompatActivity {
-
-    private final String mURL =
-            "http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange.xml";
+    private final String mURL ="http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange.xml";
+            //"http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange.xml";
     //"http://xml.nsu.ru/xml/note.xml";
     private  TextView mTextView;
+    private TextView m2TextView;
+    private String strTMP; //потом удалить
+    private String strTMP2; //потом удалить
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final HashMap<String, MoneyBox> hMap;
         mTextView = (TextView)findViewById(R.id.mTextViev);
-        //File sdPath = Environment.getExternalStorageDirectory();
-        //final String filePath = sdPath.toString() + "/1111my.xml";
+        m2TextView = (TextView)findViewById(R.id.m2textView);
         final String fileName = "mytestingxml.xml";
         mTextView.setText(fileName);
 
+//////////////////////////
+        hMap = new HashMap<String, MoneyBox>(50);
+//////////////////////////
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                  try {
+                  /*try {   //это для выкачки xml в файл на диск
                       downloadUrl(mURL, fileName);
                   } catch (IOException e) {
                       e.printStackTrace();
                   }catch (Throwable t){
                       t.printStackTrace();
                   }
+                  */
+
+                try {
+                    /*
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(true); //если надо
+                    XmlPullParser parser = factory.newPullParser();
+                    URL input = new URL(mURL); //url удаленного документа
+                    parser.setInput(input.openStream(), null);
+
+
+                    while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                        if(parser.getEventType()==XmlPullParser.START_TAG && parser.getName().
+                                equals("currency")){
+                            //MoneyBox mb = new MoneyBox();
+                            //mb.someThing = parser.getAttributeValue(0);
+                            strTMP2 += parser.getAttributeValue(0);
+                            //mb.ukrName = parser.getAttributeValue(1);
+                            strTMP2 +=parser.getAttributeValue(1);
+                            //mb.rate = parser.getAttributeValue(2);
+                            strTMP2 +=parser.getAttributeValue(2);
+                            //mb.name = parser.getAttributeValue(3);
+                            strTMP2 +=parser.getAttributeValue(3);
+                            //mb.exchangedate = parser.getAttributeValue(4);
+                            strTMP2 +=parser.getAttributeValue(4);
+                            //hMap.put(mb.name, mb);
+                            //strTMP2 ="ffff";
+                        }
+                        parser.next();
+                    */
+
+                    //следующая хуйня работает
+                    URL input = new URL(mURL);
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(true);
+                    XmlPullParser xpp = factory.newPullParser();
+                    xpp.setInput(input.openStream(), null);
+
+                    /*
+                    int eventType = xpp.getEventType();
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+                        if(eventType == XmlPullParser.START_DOCUMENT) {
+                            strTMP2 += " " + "\n";
+                        } else if(eventType == XmlPullParser.END_DOCUMENT) {
+                            strTMP2 +=" " + "\n";
+                        } else if(eventType == XmlPullParser.START_TAG) {
+                            strTMP2 +="  "+xpp.getName() + "\n";
+                        } else if(eventType == XmlPullParser.END_TAG) {
+                            strTMP2 +="  "+xpp.getName() + "\n";
+                        } else if(eventType == XmlPullParser.TEXT) {
+                            strTMP2 +=" "+xpp.getText() + "\n";
+                        }
+                        eventType = xpp.next();
+                    }
+                    */
+                    int eventType = xpp.getEventType();
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+                        if (eventType == XmlPullParser.START_TAG && xpp.getName().equals("currency")){
+                            //доделать парсинг и фильтр
+
+                        }
+                        eventType = xpp.next();
+                    }
+                }catch (Throwable e){
+                    strTMP="something error in parsing";
+                }
+
             }});
         t.start();
         int i =0;
@@ -61,33 +144,20 @@ public class MainActivity extends AppCompatActivity {
             try {
                 ++i;
                 Thread.currentThread().sleep(1000);
-                Toast.makeText(MainActivity.this, "waiting data..." + i, Toast.LENGTH_SHORT).show();
+                m2TextView.setText("waiting data..." + i);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                mTextView.setText(e.toString());
             }
         }
         Toast.makeText(MainActivity.this, "data received", Toast.LENGTH_SHORT).show();
 
-        HashMap<String, MoneyBox> hMap = new HashMap<String, MoneyBox>(50);
-        try {
-            XmlPullParser parser = getResources().getXml(R.xml.mytestingxml);
-            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
-                if(parser.getEventType()==XmlPullParser.START_TAG && parser.getName().
-                                                                        equals("currency")){
-                    MoneyBox mb = new MoneyBox();
-                    mb.someThing = parser.getAttributeValue(0);
-                    mb.ukrName = parser.getAttributeValue(1);
-                    mb.rate = parser.getAttributeValue(2);
-                    mb.name = parser.getAttributeValue(3);
-                    mb.exchangedate = parser.getAttributeValue(4);
-                    hMap.put(mb.name, mb);
-                }
-                parser.next();
+            mTextView.setText(strTMP2);
+            if(strTMP2 == "" | strTMP2== null){
+                mTextView.setText("EMPTY");
             }
-        }catch (Exception e){
-            Toast.makeText(MainActivity.this, "error in parsing", Toast.LENGTH_SHORT).show();
-        }
-        mTextView.setText(hMap.get("USD").rate);
+
+//================================================================================================//
     }
 
     private void downloadUrl(String urlStr, String file) throws IOException {
@@ -109,13 +179,5 @@ public class MainActivity extends AppCompatActivity {
             fos.close();
             bis.close();
         }
-    }
-
-    class MoneyBox{
-        String someThing;
-        String ukrName;
-        String rate;
-        String name;
-        String exchangedate;
     }
 }
